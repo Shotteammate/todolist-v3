d444d0b29856bd4e2f5f0478d5cef42b16655e5b
Tik San Pun <shotteammate@gmail.com>
2019-08-20 16:36:09 +0800

    Updates

---
+++ precache-manifest.2254c83da4706e4616a54802e0fb6ea1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62351835bd8633bd4b4372eec36ce336",
+    "url": "/Shotteammate/todolist-v3/index.html"
+  },
+  {
+    "revision": "701ab5687828068b6577",
+    "url": "/Shotteammate/todolist-v3/static/css/main.112762e7.chunk.css"
+  },
+  {
+    "revision": "02704b99fe33c47af773",
+    "url": "/Shotteammate/todolist-v3/static/js/2.b5a53c56.chunk.js"
+  },
+  {
+    "revision": "701ab5687828068b6577",
+    "url": "/Shotteammate/todolist-v3/static/js/main.f973b3d2.chunk.js"
+  },
+  {
+    "revision": "f8c9d8eb9f9c78f8e05f",
+    "url": "/Shotteammate/todolist-v3/static/js/runtime~main.9c66a083.js"
+  }
+]);
