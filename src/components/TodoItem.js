316cc01a053f4945d295da9f7553e4c664b8d7f6
Tik San Pun <shotteammate@gmail.com>
2019-08-22 11:46:31 +0800

    modify flexbox spacing

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -8,7 +8,8 @@ export class TodoItem extends Component {
       borderBottom:'1px #ccc solid',
       textDecoration: this.props.todo.completed? 'line-through': 'none',
       display: 'flex',
-      justifyContent:'space-around',
+      flexDirection: 'row',
+      justifyContent:'flex-start',
     }
   }
 
@@ -17,14 +18,14 @@ export class TodoItem extends Component {
 
     return (
       <div style={this.getStyle()}>
-        <div style={{flexGrow:1}}>
+        <div >
           <input 
             type="checkbox"
             checked={completed}
             onChange ={this.props.markComplete.bind(this,id)}/>
         </div>
         <div style={titleStyle}>{title}</div>
-        <div style={{flexGrow:1}}>
+        <div style={{marginLeft: 'auto'}}>
           <button 
             className="delBtnStyle"
             onClick={this.props.deleteTodo.bind(this,id)}
@@ -36,7 +37,7 @@ export class TodoItem extends Component {
 }
 
 const titleStyle = {
-  flexGrow:2, 
+ 
   display: 'inline-block',
   wordBreak: 'break-word',
   margin: '0 10px'
